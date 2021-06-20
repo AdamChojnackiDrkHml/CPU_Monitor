92e2d2a6c08e101f296fae7453f2b7e938afc3e2
Adam <niepoprawnyetycznie@gmail.com>
2021-06-20 16:33:09 +0200

    init Analyzer structures

---
+++ headers/analyzer.h
@@ -0,0 +1,6 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+extern void*  analyzer_task(void *arg);
+
+#endif
---
+++ src/analyzer.c
@@ -0,0 +1,7 @@
+#include "../headers/analyzer.h"
+#include <stdio.h>
+
+extern void* analyzer_task(void *arg)
+{
+
+}
