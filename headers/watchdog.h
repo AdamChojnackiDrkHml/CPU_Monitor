499f916eae87bf440195fcbce9b14d6cfd4cfbef
Adam <niepoprawnyetycznie@gmail.com>
2021-06-24 16:18:08 +0200

    remove magic variables

--- headers/watchdog.h
+++ headers/watchdog.h
@@ -1,6 +1,7 @@
 #ifndef WATCHDOG_H
 #define WATCHDOG_H
 
+
 extern void* watchdog_task(void* arg);
 extern void watchdog_call_exit(void);
 extern void watchdog_set_me_alive(unsigned char ID);
--- src/watchdog.c
+++ src/watchdog.c
@@ -3,43 +3,53 @@
 #include <unistd.h>
 #include "../headers/global.h"
 
-
+static const size_t watchdog_check_time = 2;
+static const size_t watchdog_num_of_threads_to_watch = 4;
 static volatile size_t watchdog_control = 1;
 static const size_t watchdog_control_end = 0;
-    
-static volatile size_t end_succes = 0;
-static unsigned char state = 1;
-static volatile _Atomic(char) watchdog_check[NUM_OF_THREADS];
+
+static const char thread_state_unknown = 1;
+static const char thread_state_alive = 0;     
+
+static const char watchdog_all_threads_alive = 1;
+static const char watchdog_thread_is_dead = 1;
+
+
+
+static volatile size_t end_state = THREAD_WORKING;
+static unsigned char state = watchdog_all_threads_alive;
+static volatile _Atomic(char) watchdog_check[watchdog_num_of_threads_to_watch];
  
 void* watchdog_task(void* arg)
 {
 
     while(watchdog_control)
     {
-        sleep(2);
-        for(size_t i = 0; i < NUM_OF_THREADS; i++)
+        sleep(watchdog_check_time);
+        for(size_t i = 0; i < watchdog_num_of_threads_to_watch; i++)
         {
-            if(!watchdog_check[i])
+            if(watchdog_check[i] == thread_state_unknown)
             {
-                state = 0;
+                state = watchdog_thread_is_dead;
                 break;
             }
-            watchdog_check[i] = 0;
+            watchdog_check[i] = thread_state_unknown;
         }
     }
     while(watchdog_control);
-    end_succes = 1;
+    end_state = THREAD_END;
+    return NULL;
 }
 
 void watchdog_call_exit(void)
 {
-    watchdog_control = watchdog_control_end;
-    while(!end_succes);
+    watchdog_control = END_THREAD;
+    while(end_state == THREAD_WORKING);
 }
 
 void watchdog_set_me_alive(unsigned char ID)
 {
-    watchdog_check[ID] = 1;
+    watchdog_check[ID] = thread_state_alive;
 }
 
 unsigned char watchdog_raport(void)
